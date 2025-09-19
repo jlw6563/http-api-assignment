3ddbf577b88add51b8d9fdf7a40387b233b8ef91
Jake Wardell <jlw6563@rit.edu>
2025-09-19 14:44:45 -0400

    Pushing classwork

--- src/server.js
+++ src/server.js
@@ -7,23 +7,19 @@ const urlStruct = {
 };
 
 const onRequest = (request, response) => {
-    const protocol = request.connection.encrypted ? 'https' : 'http';
-    const parsedURL = new URL(request.url, `${protocol}://${request.headers.host}`);
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedURL = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-    request.acceptedTypes = request.headers.accept.split(',');
-    request.query = Object.fromEntries(parsedURL.searchParams);
+  request.acceptedTypes = request.headers.accept.split(',');
+  request.query = Object.fromEntries(parsedURL.searchParams);
 
-    if (urlStruct[parsedURL.pathname]) {
-        urlStruct[parsedURL.pathname](request, response);
-    } else {
-        // Makes this a 404
-    }
+  if (urlStruct[parsedURL.pathname]) {
+    urlStruct[parsedURL.pathname](request, response);
+  } else {
+    // Makes this a 404
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
-
-
-    console.log(`Listening on port: ${port}`);
-
-
+  console.log(`Listening on port: ${port}`);
 });
